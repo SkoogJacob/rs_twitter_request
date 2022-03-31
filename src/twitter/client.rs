bbbf825bcc703919fb81650ce2158bf32fe40a2d
Jacob Skoog <jacob.skoog@protonmail.com>
2022-04-01 00:40:27 +0200

    working on the client itself

--- src/twitter/client.rs
+++ src/twitter/client.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use super::endpoints::Endpoint;
 use http::Method;
 /**
@@ -5,5 +6,35 @@ This module will have to contain the requests, and then you can break up the fil
 to other files to keep things neat.
  */
 pub struct TwitterClient{
+    client: reqwest::Client,
+    groups: BTreeSet<Group>
+}
 
+struct Group {
+}
+
+struct Filter<'a> {
+    f_type: FilterType,
+    key: Option<&'a str>,
+    value: &'a str
+}
+
+impl Filter {
+    fn to_string(&self) -> String {
+        let mut s = String::new();
+        match self.key {
+            None => { s.insert_str(0, self.value); }
+            Some(k) => {
+                s.insert_str(0, k);
+                s.insert_str(s.len(), ":");
+                s.insert_str(s.len(), self.value);
+            }
+        }
+        s
+    }
+}
+
+enum FilterType{
+    Main,
+    Secondary
 }
