ac40f26fddc573d586a7f441ae81c312bbf1728c
Jacob Skoog <jacob.skoog@protonmail.com>
2022-04-05 01:39:20 +0200

    added some documentation and improved display for PointRadius

--- src/twitter/query_params/filter.rs
+++ src/twitter/query_params/filter.rs
@@ -49,39 +49,39 @@ impl<'a> Display for Filter<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let (mut filter_string, is) = match self {
             Filter::Keyword(val, exact, is) => {
-                let string = if exact.val() {
+                let string = if exact.eval() {
                     // If `exact`, wrap with `"`
                     format!("\"{}\"", val)
                 } else {
                     val.to_string()
                 };
-                (string, is.val())
+                (string, is.eval())
             }
-            Filter::From(val, is) => (format!("from:{}", val), is.val()),
-            Filter::RetweetsOf(val, is) => (format!("retweets_of:{}", val), is.val()),
-            Filter::Context(val, is) => (format!("{}{}", "context:", val), is.val()),
-            Filter::Entity(val, is) => (format!("{}{}{}", "entity:\"", val, "\""), is.val()),
-            Filter::Url(val, is) => (format!("{}{}{}", "url:\"", val, "\""), is.val()),
-            Filter::To(val, is) => (format!("{}{}", "to:", val), is.val()),
-            Filter::IsRetweet(is) => (String::from("is:retweet"), is.val()),
-            Filter::IsReply(is) => (String::from("is:reply"), is.val()),
-            Filter::IsQuote(is) => (String::from("is:quote"), is.val()),
-            Filter::IsVerified(is) => (String::from("is:verified"), is.val()),
-            Filter::IsNullcast(is) => (String::from("is:nullcast"), is.val()),
-            Filter::HasHashtags(is) => (String::from("has:hashtags"), is.val()),
-            Filter::HasCashtags(is) => (String::from("has:cashtags"), is.val()),
-            Filter::HasLinks(is) => (String::from("has:links"), is.val()),
-            Filter::HasMentions(is) => (String::from("has:mentions"), is.val()),
-            Filter::HasMedia(is) => (String::from("has:media"), is.val()),
-            Filter::HasImages(is) => (String::from("has:images"), is.val()),
-            Filter::HasVideos(is) => (String::from("has:videos"), is.val()),
-            Filter::HasGeo(is) => (String::from("has:geo"), is.val()),
-            Filter::LocPlaceCountry(val, is) => {(format!("place_country:{}", val), is.val())}
-            Filter::LocPlace(val, is) => {(format!("place:\"{}\"", val), is.val())}
-            Filter::LocBoundingBox(bound_box, is) => {(format!("bounding_box:{}", bound_box), is.val())}
-            Filter::LocPointRadius(point_radius, is) => {(format!("point_radius:{}", point_radius), is.val())}
-            Filter::LangLang(lang, is) => {(format!("lang:{}", lang), is.val())}
-            Filter::ConvConversationId(id, is) => {(format!("conversation_id:{}", id), is.val())}
+            Filter::From(val, is) => (format!("from:{}", val), is.eval()),
+            Filter::RetweetsOf(val, is) => (format!("retweets_of:{}", val), is.eval()),
+            Filter::Context(val, is) => (format!("{}{}", "context:", val), is.eval()),
+            Filter::Entity(val, is) => (format!("{}{}{}", "entity:\"", val, "\""), is.eval()),
+            Filter::Url(val, is) => (format!("{}{}{}", "url:\"", val, "\""), is.eval()),
+            Filter::To(val, is) => (format!("{}{}", "to:", val), is.eval()),
+            Filter::IsRetweet(is) => (String::from("is:retweet"), is.eval()),
+            Filter::IsReply(is) => (String::from("is:reply"), is.eval()),
+            Filter::IsQuote(is) => (String::from("is:quote"), is.eval()),
+            Filter::IsVerified(is) => (String::from("is:verified"), is.eval()),
+            Filter::IsNullcast(is) => (String::from("is:nullcast"), is.eval()),
+            Filter::HasHashtags(is) => (String::from("has:hashtags"), is.eval()),
+            Filter::HasCashtags(is) => (String::from("has:cashtags"), is.eval()),
+            Filter::HasLinks(is) => (String::from("has:links"), is.eval()),
+            Filter::HasMentions(is) => (String::from("has:mentions"), is.eval()),
+            Filter::HasMedia(is) => (String::from("has:media"), is.eval()),
+            Filter::HasImages(is) => (String::from("has:images"), is.eval()),
+            Filter::HasVideos(is) => (String::from("has:videos"), is.eval()),
+            Filter::HasGeo(is) => (String::from("has:geo"), is.eval()),
+            Filter::LocPlaceCountry(val, is) => {(format!("place_country:{}", val), is.eval())}
+            Filter::LocPlace(val, is) => {(format!("place:\"{}\"", val), is.eval())}
+            Filter::LocBoundingBox(bound_box, is) => {(format!("bounding_box:{}", bound_box), is.eval())}
+            Filter::LocPointRadius(point_radius, is) => {(format!("point_radius:{}", point_radius), is.eval())}
+            Filter::LangLang(lang, is) => {(format!("lang:{}", lang), is.eval())}
+            Filter::ConvConversationId(id, is) => {(format!("conversation_id:{}", id), is.eval())}
         };
         if !is {
             filter_string = format!("{}{}", "-", filter_string);
@@ -91,22 +91,25 @@ impl<'a> Display for Filter<'a> {
 }
 
 trait Val: Sized {
-    fn val(&self) -> bool;
+    fn eval(&self) -> bool;
 }
+/// Simple named wrapper for a boolean that indicates if select search terms are to be "exact".
 #[derive(PartialEq, Eq)]
 pub struct Exact(bool);
 impl Val for Exact {
-    fn val(&self) -> bool {
+    fn eval(&self) -> bool {
         self.0
     }
 }
+/// Simple named wrapper for a boolean that says whether a filter is positive (shall match) or negative (shall not match)
 #[derive(PartialEq, Eq)]
 pub struct Is(bool);
 impl Val for Is {
-    fn val(&self) -> bool {
+    fn eval(&self) -> bool {
         self.0
     }
 }
+/// A struct for a bounding box location search, having two coordinate pairs
 #[derive(PartialEq, Debug)]
 pub struct BoundingBox(pub f32, pub f32, pub f32, pub f32);
 impl Display for BoundingBox {
@@ -114,14 +117,16 @@ impl Display for BoundingBox {
         write!(f, "[{:.6} {:.6} {:.6} {:.6}]", self.0, self.1, self.2, self.3)
     }
 }
+/// A struct for a point+radius location search, having a coordinate pair for the
+/// circle center, and a radius expressed in km.
 #[derive(PartialEq, Debug)]
 pub struct PointRadius {
     pub longitude: f32,
     pub latitude: f32,
-    pub radius: u32
+    pub radius_km: u32
 }
 impl Display for PointRadius{
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{:.6} {:.6} {}]", self.longitude, self.latitude, self.radius)
+        write!(f, "[{:.6} {:.6} {}km]", self.longitude, self.latitude, self.radius_km)
     }
 }
