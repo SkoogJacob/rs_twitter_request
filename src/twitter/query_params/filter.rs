112a5225f62c30e151b4b65136d16fe492611ad0
Jacob Skoog <jacob.skoog@protonmail.com>
2022-05-06 08:00:00 +0200

    Changed Is and Exact structs to be Enums.

--- src/twitter/query_params/filter.rs
+++ src/twitter/query_params/filter.rs
@@ -47,71 +47,70 @@ impl<'a> Filter<'a> {
 
 impl<'a> Display for Filter<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let (mut filter_string, is) = match self {
+        let (filter_string, is) = match self {
             Filter::Keyword(val, exact, is) => {
-                let string = if exact.eval() {
+                let string = match exact {
                     // If `exact`, wrap with `"`
-                    format!("\"{}\"", val)
-                } else {
-                    val.to_string()
+                    Exact::Is => {format!("\"{}\"", val)}
+                    Exact::Not => {val.to_string()}
                 };
-                (string, is.eval())
+                (string, is)
             }
-            Filter::From(val, is) => (format!("from:{}", val), is.eval()),
-            Filter::RetweetsOf(val, is) => (format!("retweets_of:{}", val), is.eval()),
-            Filter::Context(val, is) => (format!("context:{}", val), is.eval()),
-            Filter::Entity(val, is) => (format!("entity:\"{}\"", val), is.eval()),
-            Filter::Url(val, is) => (format!("url:\"{}\"", val), is.eval()),
-            Filter::To(val, is) => (format!("to:{}", val), is.eval()),
-            Filter::IsRetweet(is) => (String::from("is:retweet"), is.eval()),
-            Filter::IsReply(is) => (String::from("is:reply"), is.eval()),
-            Filter::IsQuote(is) => (String::from("is:quote"), is.eval()),
-            Filter::IsVerified(is) => (String::from("is:verified"), is.eval()),
-            Filter::IsNullcast(is) => (String::from("is:nullcast"), is.eval()),
-            Filter::HasHashtags(is) => (String::from("has:hashtags"), is.eval()),
-            Filter::HasCashtags(is) => (String::from("has:cashtags"), is.eval()),
-            Filter::HasLinks(is) => (String::from("has:links"), is.eval()),
-            Filter::HasMentions(is) => (String::from("has:mentions"), is.eval()),
-            Filter::HasMedia(is) => (String::from("has:media"), is.eval()),
-            Filter::HasImages(is) => (String::from("has:images"), is.eval()),
-            Filter::HasVideos(is) => (String::from("has:videos"), is.eval()),
-            Filter::HasGeo(is) => (String::from("has:geo"), is.eval()),
-            Filter::LocPlaceCountry(val, is) => (format!("place_country:{}", val), is.eval()),
-            Filter::LocPlace(val, is) => (format!("place:\"{}\"", val), is.eval()),
+            Filter::From(val, is) => (format!("from:{}", val), is),
+            Filter::RetweetsOf(val, is) => (format!("retweets_of:{}", val), is),
+            Filter::Context(val, is) => (format!("context:{}", val), is),
+            Filter::Entity(val, is) => (format!("entity:\"{}\"", val), is),
+            Filter::Url(val, is) => (format!("url:\"{}\"", val), is),
+            Filter::To(val, is) => (format!("to:{}", val), is),
+            Filter::IsRetweet(is) => (String::from("is:retweet"), is),
+            Filter::IsReply(is) => (String::from("is:reply"), is),
+            Filter::IsQuote(is) => (String::from("is:quote"), is),
+            Filter::IsVerified(is) => (String::from("is:verified"), is),
+            Filter::IsNullcast(is) => (String::from("is:nullcast"), is),
+            Filter::HasHashtags(is) => (String::from("has:hashtags"), is),
+            Filter::HasCashtags(is) => (String::from("has:cashtags"), is),
+            Filter::HasLinks(is) => (String::from("has:links"), is),
+            Filter::HasMentions(is) => (String::from("has:mentions"), is),
+            Filter::HasMedia(is) => (String::from("has:media"), is),
+            Filter::HasImages(is) => (String::from("has:images"), is),
+            Filter::HasVideos(is) => (String::from("has:videos"), is),
+            Filter::HasGeo(is) => (String::from("has:geo"), is),
+            Filter::LocPlaceCountry(val, is) => (format!("place_country:{}", val), is),
+            Filter::LocPlace(val, is) => (format!("place:\"{}\"", val), is),
             Filter::LocBoundingBox(bound_box, is) => {
-                (format!("bounding_box:{}", bound_box), is.eval())
+                (format!("bounding_box:{}", bound_box), is)
             }
             Filter::LocPointRadius(point_radius, is) => {
-                (format!("point_radius:{}", point_radius), is.eval())
+                (format!("point_radius:{}", point_radius), is)
             }
-            Filter::LangLang(lang, is) => (format!("lang:{}", lang), is.eval()),
-            Filter::ConvConversationId(id, is) => (format!("conversation_id:{}", id), is.eval()),
+            Filter::LangLang(lang, is) => (format!("lang:{}", lang), is),
+            Filter::ConvConversationId(id, is) => (format!("conversation_id:{}", id), is),
         };
-        if !is {
-            filter_string = format!("-{}", filter_string);
+        match is {
+            Is::Is => {
+                write!(f, "{}", filter_string)
+            }
+            Is::Not => {
+                write!(f, "-{}", filter_string)
+            }
         }
-        write!(f, "{}", filter_string)
     }
 }
 
 trait Val: Sized {
     fn eval(&self) -> bool;
 }
-/// Simple named wrapper for a boolean that indicates if select search terms are to be "exact".
+/// This enum is used to indicate if a filter is meant to be exact or not
 #[derive(PartialEq, Eq, Debug)]
-pub struct Exact(bool);
-impl Val for Exact {
-    fn eval(&self) -> bool {
-        self.0
-    }
+pub enum Exact {
+    Is,
+    Not
 }
-/// Simple named wrapper for a boolean that says whether a filter is positive (shall match) or negative (shall not match)
+/// This enum is used to indicate if a filter tests for existence or absence
 #[derive(PartialEq, Eq, Debug)]
-pub struct Is(bool);
-impl Val for Is {
-    fn eval(&self) -> bool {
-        self.0
-    }
+pub enum Is {
+    Is,
+    Not
 }
 /// A struct for a bounding box location search, having two coordinate pairs
 #[derive(PartialEq, Debug)]
